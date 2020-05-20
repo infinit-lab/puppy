8748ce61a60087e9779a04195ccb8850395b8371
infinit-lab <infinit-lab@github.com>
2020-05-20 13:50:18 +0800

    fix can't wait process in linux

---
+++ src/controller/process/guard_linux.go
@@ -0,0 +1,15 @@
+package process
+
+type guard struct {
+}
+
+func (g *guard) run() {
+
+}
+
+type slave struct {
+}
+
+func (s *slave) run() {
+
+}
--- src/controller/process/manager.go
+++ src/controller/process/manager.go
@@ -200,7 +200,7 @@ func (m *manager) start(p *processData) error {
 			updateProcessStatus(p, true, p.cmd.Process.Pid)
 
 			var wg sync.WaitGroup
-			// file, _ := m.storeLog(p, &wg)
+			file, _ := m.storeLog(p, &wg)
 
 			if err := p.cmd.Wait(); err != nil {
 				logutils.WarningF("%s quit. error: %v", p.process.Name, err)
@@ -235,7 +235,7 @@ func (m *manager) stop(p *processData) error {
 		return nil
 	}
 	p.isStart = false
-	logutils.ErrorF("%s isStart is %v", p.process.Name, p.isStart)
+	logutils.TraceF("%s isStart is %v", p.process.Name, p.isStart)
 	if p.cmd == nil {
 		return nil
 	}
